dcd44a149b41fc48be7456adf09c599d236f1fab
Daniele Madama <d.madama@gmail.com>
2017-01-16 23:57:22 +0100

    Replace jcurses with lanterna

--- src/main/java/org/danysoft/ev3rpi/RobotUI.java
+++ src/main/java/org/danysoft/ev3rpi/RobotUI.java
@@ -3,18 +3,26 @@ package org.danysoft.ev3rpi;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-import jcurses.event.WindowEvent;
-import jcurses.event.WindowListener;
-import jcurses.widgets.BorderPanel;
-import jcurses.widgets.GridLayoutManager;
-import jcurses.widgets.Window;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.Button;
+import com.googlecode.lanterna.gui2.DefaultWindowManager;
+import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.GridLayout;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.TextBox;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
-public class RobotUI extends Window implements WindowListener {
+public class RobotUI {
 
 	public static void main(String[] args) {
-		Window ui = new RobotUI(28, 20);
-		ui.addListener((WindowListener) ui);
-		ui.show();
+		RobotUI ui = new RobotUI();
 	}
 
 	private String propertiesPath = "ev3rpi.properties";
@@ -22,16 +30,44 @@ public class RobotUI extends Window implements WindowListener {
 	private OpenCVUtils ocv;
 	private CamUtils cam;
 
-	public RobotUI(int width, int height) {
-		super(width, height, true, "EV3 RPI");
+	public RobotUI() {
 		try {
 			init();
+
+			// Setup terminal and screen layers
+			Terminal terminal = new DefaultTerminalFactory().createTerminal();
+			Screen screen = new TerminalScreen(terminal);
+			screen.startScreen();
+
+			// Create panel to hold components
+			Panel panel = new Panel();
+			panel.setLayoutManager(new GridLayout(2));
+
+			panel.addComponent(new Label("Forename"));
+			panel.addComponent(new TextBox());
+
+			panel.addComponent(new Label("Surname"));
+			panel.addComponent(new TextBox());
+
+			panel.addComponent(new EmptySpace(new TerminalSize(0, 0))); // Empty
+																		// space
+																		// underneath
+																		// labels
+			panel.addComponent(new Button("Submit"));
+
+			// Create window to hold the panel
+			BasicWindow window = new BasicWindow();
+			window.setComponent(panel);
+
+			// Create gui and start gui
+			MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(),
+					new EmptySpace(TextColor.ANSI.BLUE));
+			gui.addWindowAndWait(window);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		BorderPanel bp = new BorderPanel();
-		GridLayoutManager manager = new GridLayoutManager(2, 5);
-		bp.setLayoutManager(manager);
+
+
 	}
 
 	private void init() throws Exception {
@@ -40,13 +76,6 @@ public class RobotUI extends Window implements WindowListener {
 		properties.load(new FileInputStream(propertiesPath));
 		if (properties.containsKey("webcam.skipframes")) {
 			cam.setSkipFrames(Integer.valueOf(properties.getProperty("webcam.skipframes")));
-		}
-	}
-
-	@Override
-	public void windowChanged(WindowEvent event) {
-		if (event.getType() == WindowEvent.CLOSING) {
-			event.getSourceWindow().close();
 		}
 	}
 
